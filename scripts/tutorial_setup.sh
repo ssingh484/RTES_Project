8964518efc6aef9a723e8098ff11c8eecb4af82e
Rodrigo A. Lima <ral@gatech.edu>
2021-05-01 15:47:29 -0400

    Integrate microbenchmark to the experiment worfklow.

--- scripts/tutorial_setup.sh
+++ scripts/tutorial_setup.sh
@@ -7,6 +7,9 @@
 set -u
 while [[ $# > 1 ]]; do
   case $1 in
+    --type )
+      type=$2
+      ;;
     --username )
       username=$2
       ;;
@@ -36,6 +39,11 @@ done
 # Copy the SSH private key to node-0.
 scp -o StrictHostKeyChecking=no ${private_ssh_key_path} ${username}@${node_0}:.ssh/id_rsa
 
+if [ $type = microbench ]; then
+  node_2=empty
+  node_3=empty
+fi
+
 # Log into node-0.
 ssh -o StrictHostKeyChecking=no ${username}@${node_0} "
   # Install Docker.
@@ -53,10 +61,21 @@ ssh -o StrictHostKeyChecking=no ${username}@${node_0} "
   mv BuzzBlogBenchmark/controller/conf/* .
   rm -rf BuzzBlogBenchmark
 
-  # Replace variables in system.yml.
-  sed -i \"s/{{username}}/${username}/g\" system.yml
-  sed -i \"s/{{node-0}}/${node_0}/g\" system.yml
-  sed -i \"s/{{node-1}}/${node_1}/g\" system.yml
-  sed -i \"s/{{node-2}}/${node_2}/g\" system.yml
-  sed -i \"s/{{node-3}}/${node_3}/g\" system.yml
+  # Set up configuration files.
+  if [ $type = microbench ]; then
+    mv system_microbench.yml system.yml
+    rm system_loadgen.yml
+    rm workload.yml
+    sed -i \"s/{{username}}/${username}/g\" system.yml
+    sed -i \"s/{{node-0}}/${node_0}/g\" system.yml
+    sed -i \"s/{{node-1}}/${node_1}/g\" system.yml
+  else
+    mv system_loadgen.yml system.yml
+    rm system_microbench.yml
+    sed -i \"s/{{username}}/${username}/g\" system.yml
+    sed -i \"s/{{node-0}}/${node_0}/g\" system.yml
+    sed -i \"s/{{node-1}}/${node_1}/g\" system.yml
+    sed -i \"s/{{node-2}}/${node_2}/g\" system.yml
+    sed -i \"s/{{node-3}}/${node_3}/g\" system.yml
+  fi
 "
